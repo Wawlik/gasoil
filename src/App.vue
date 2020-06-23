f51fb17ff0f3fffd6d50452c7ecf035003eeef3a
wawlik <craazzy777@gmail.com>
2020-06-23 13:39:33 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import main from './components/main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    main
   }
 }
 </script>
